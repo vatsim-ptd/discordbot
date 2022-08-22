5ffc2f5f657bfb25769247b756c5c04bfdb94ba6
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-08-21 19:23:19 -0700

    first commit

---
+++ events/ready.js
@@ -0,0 +1,9 @@
+const { ActivityType } = require('discord.js');
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(client) {
+        console.log(`Logged in as ${client.user.tag}`);
+        client.user.setActivity('The VATSIM Network', { type: ActivityType.Watching })
+    },
+};
