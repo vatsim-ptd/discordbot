228fe4d6d719098f01f0489427eb0b125fe90113
thepilotclub <webdev@thepilotclub.net>
2022-11-06 13:54:56 -0600

    new install

--- commands/mass-roles.js
+++ commands/mass-roles.js
@@ -6,10 +6,9 @@ module.exports = {
         .setName('mass-roles')
         .setDescription('Assigns your VATSIM Rating Roles!'),
     async execute(interaction) {
-        const guild = interaction.client.guilds.cache.get("1037908270737784872");
-        const members = await guild.members.fetch();
-        members.forEach(member => assignRoles(member));
-
+            const guild = interaction.client.guilds.cache.get("1037908270737784872");
+            const members = await interaction.guild.members.fetch();
+            members.forEach(member => assignRoles(member));
         await interaction.reply({content: 'Done!', ephemeral: true}).catch(error =>
             console.error`I failed at the edit reply stage`)
     }
